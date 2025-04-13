be23f906001a41c07b35b7ea77202c43133b7bd6
Nickchen Nick <nickchen120235@gmail.com>
2025-04-13 16:07:23 +0800

    init

---
+++ common.ts
@@ -0,0 +1,18 @@
+export { parse } from "node-html-parser";
+
+export const HEADER = {
+  "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+}
+
+export class HttpError extends Error {
+  res: Response
+  constructor(res: Response) {
+    super(`Server returned ${res.status} when fetching ${res.url}`)
+    this.name = this.constructor.name
+    this.res = res
+  }
+}
+
+export const kv = await Deno.openKv();
+
+export const DISCORD_WEBHOOK = Deno.env.get("DISCORD_WEBHOOK") ?? "";
---
+++ nako.ts
@@ -0,0 +1,61 @@
+import { parse, HEADER, HttpError, kv, DISCORD_WEBHOOK } from "./common.ts";
+
+const base = "https://account.tonarininako.jp";
+
+interface Post {
+  url: string;
+  title: string;
+  time: string;
+  _id: number;
+}
+
+async function nako(): Promise<void> {
+  const res = await fetch(`${base}/login.php`, { headers: HEADER });
+  if (!res.ok)
+    throw new HttpError(res);
+  const html = await res.text();
+  const root = parse(html);
+  const news = root.querySelectorAll("section#news > ul > li.news__li");
+  console.log(`Found ${news.length} post`)
+  const posts: Post[] = [];
+  for (const n of news) {
+    const href = n.querySelector("a")?.getAttribute("href");
+    const title = n.querySelector("div.news__title")?.innerText.trim();
+    const time = n.querySelector("time")?.innerText.trim();
+    if (href && title && time) {
+      const link = new URL(href, base);
+      const id = link.searchParams.get("id");
+      if (id) {
+        posts.push({
+          url: link.href,
+          title,
+          time,
+          _id: parseInt(id),
+        })
+      }
+    }
+  }
+  console.log(posts)
+  const lastPost = (await kv.get<number>(["post", "nako"])).value ?? 0;
+  if (lastPost !== 0) {
+    // sort posts by _id from small to big
+    for (const p of posts.toSorted((a, b) => a._id - b._id)) {
+      if (p._id <= lastPost) continue;
+      const _ = await fetch(DISCORD_WEBHOOK, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "となりになこ",
+          content: `## ${p.title}\n${p.time}\n${p.url}`,
+        }),
+      })
+    }
+  }
+  if (posts[0]._id > lastPost) {
+    await kv.set(["post", "nako"], posts[0]._id);
+  }
+}
+
+await nako();
