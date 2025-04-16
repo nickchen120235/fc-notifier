24047f9d4ef57b28f453ffc8d4ee46b359f5cd20
Nickchen Nick <nickchen120235@gmail.com>
2025-04-16 23:55:18 +0800

    use url instead of post id

--- liella.ts
+++ liella.ts
@@ -2,6 +2,8 @@ import { parse, HEADER, HttpError, kv, DISCORD_WEBHOOK, type Post } from "./comm
 
 const base = "https://lovelive-liellaclub.jp";
 
+type LiellaPost = Omit<Post, "_id">
+
 async function liella(): Promise<void> {
   const res = await fetch(`${base}/mob/index.php?site=LC&ima=5622`, { headers: HEADER });
   if (!res.ok)
@@ -10,7 +12,7 @@ async function liella(): Promise<void> {
   const root = parse(html);
   const news = root.querySelectorAll("div.entry_list > div.entry_panel");
   console.log(`Found ${news.length} post`);
-  const posts: Post[] = [];
+  const posts: LiellaPost[] = [];
   for (const n of news) {
     const href = n.querySelector("a")?.getAttribute("href");
     const title = n.querySelector("div.entry_text > p")?.innerText.trim();
@@ -23,31 +25,28 @@ async function liella(): Promise<void> {
           url: link.href,
           title,
           time,
-          _id: parseInt(id),
         })
       }
     }
   }
   console.log(posts);
-  const lastPost = (await kv.get<number>(["post", "liella"])).value ?? 0;
-  if (lastPost !== 0) {
-    // sort posts by _id from small to big
-    for (const p of posts.toSorted((a, b) => a._id - b._id)) {
-      if (p._id <= lastPost) continue;
-      const _ = await fetch(DISCORD_WEBHOOK, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          username: "Liella! CLUB",
-          content: `## ${p.title}\n${p.time}\n${p.url}`,
-        }),
-      })
-    }
+  const lastPost = (await kv.get<string>(["post", "liella"])).value ?? "";
+  // use copy of reversed posts
+  for (const p of posts.toReversed()) {
+    if (p.url === lastPost) break;
+    await fetch(DISCORD_WEBHOOK, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: "Liella! CLUB",
+        content: `## ${p.title}\n${p.time}\n${p.url}`,
+      }),
+    })
   }
-  if (posts[0]._id > lastPost) {
-    await kv.set(["post", "liella"], posts[0]._id);
+  if (posts[0].url !== lastPost) {
+    await kv.set(["post", "liella"], posts[0].url);
   }
 }
 
