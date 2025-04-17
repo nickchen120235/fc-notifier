80527a140427ed0746337effccbee5eddd574022
Nickchen Nick <nickchen120235@gmail.com>
2025-04-17 16:32:46 +0800

    try title and time as key

--- liella.ts
+++ liella.ts
@@ -2,35 +2,34 @@ import { parse, HEADER, HttpError, kv, DISCORD_WEBHOOK, type Post } from "./comm
 
 const base = "https://lovelive-liellaclub.jp";
 
+type LiellaPost = Omit<Post, "_id">
+
 async function liella(): Promise<void> {
-  const res = await fetch(`${base}/mob/index.php?site=LC&ima=5622`, { headers: HEADER });
+  const res = await fetch(`${base}/mob/index.php`, { headers: HEADER });
   if (!res.ok)
     throw new HttpError(res);
   const html = await res.text();
   const root = parse(html);
   const news = root.querySelectorAll("div.entry_list > div.entry_panel");
   console.log(`Found ${news.length} post`);
-  const posts: Post[] = [];
+  const posts: LiellaPost[] = [];
   for (const n of news) {
     const href = n.querySelector("a")?.getAttribute("href");
     const title = n.querySelector("div.entry_text > p")?.innerText.trim();
     const time = n.querySelector("div.entry_label > span.date")?.innerText.trim();
     if (href && title && time) {
       const link = new URL(href, base);
-      const id = link.searchParams.get("id");
-      if (id) {
-        posts.push({
-          url: link.href,
-          title,
-          time,
-          _id: parseInt(id),
-        })
-      }
+      posts.push({
+        url: link.href,
+        title,
+        time,
+      })
     }
   }
   console.log(posts);
   for (const p of posts.toReversed()) {
-    if ((await kv.get<string>(["post", "liella", p.time, p._id])).value !== null) continue;
+    console.log((await kv.get<string>(["post", "liella", p.time, p.title])).value);
+    if ((await kv.get<string>(["post", "liella", p.time, p.title])).value !== null) continue;
     const _ = await fetch(DISCORD_WEBHOOK, {
       method: "POST",
       headers: {
@@ -41,8 +40,9 @@ async function liella(): Promise<void> {
         content: `## ${p.title}\n${p.time}\n${p.url}`,
       }),
     })
-    await kv.set(["post", "liella", p.time, p._id], p.title, { expireIn: 30 * 86400 });
+    await kv.set(["post", "liella", p.time, p.title], p.title, { expireIn: 30 * 86400 });
   }
 }
 
-export default liella;
+// export default liella;
+await liella();
